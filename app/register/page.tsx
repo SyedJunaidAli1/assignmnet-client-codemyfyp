271a0f5e543fa27b533f7d8a1d6d09de05e43f9e
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-06 23:35:14 +0530

    feat: initial commit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,97 @@
-const Register = () => {
-  
-  return (
-    <>
-      reg page
-    </>
-  )
-};
+"use client";
 
-export default Register;
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import api from "@/app/lib/api";
+
+export default function RegisterPage() {
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError("");
+
+      await api.post("/auth/register", form);
+
+      router.push("/login");
+    } catch (err: any) {
+      setError(err.response?.data?.message || "Registration failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center  p-4">
+      <div className="w-full max-w-md rounded-xl p-6 shadow-lg">
+        <h1 className="mb-6 text-2xl font-semibold text-center">
+          Create Account
+        </h1>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            name="name"
+            placeholder="Full Name"
+            onChange={handleChange}
+            required
+            className="w-full rounded-md border p-3"
+          />
+
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            required
+            className="w-full rounded-md border p-3"
+          />
+
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+            className="w-full rounded-md border p-3"
+          />
+
+          {error && <p className="text-sm text-red-500">{error}</p>}
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full rounded-md bg-black text-white py-3 hover:bg-gray-800 transition"
+          >
+            {loading ? "Creating..." : "Register"}
+          </button>
+        </form>
+
+        <p className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <a href="/login" className="text-blue-600">
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
