92cc26a00803e5f710446aa86136d9dc7943b52f
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-06 14:26:49 +0530

    auth.ts and changes in api.tsx

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
